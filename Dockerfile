1951a5e087c27c7ccb5f046188e6c768f9968c90
Yehiyam Livneh <yehiyam@gmail.com>
2019-07-01 13:48:48 +0300

    use private build of kaniko until GoogleContainerTools/kaniko#683 is merged

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM gcr.io/kaniko-project/executor:debug-v0.10.0
+# FROM gcr.io/kaniko-project/executor:debug-v0.10.0
+FROM yehiyam/executor:debug
 COPY run.sh /scripts/
 ENTRYPOINT [ "sh","-c", "/scripts/run.sh" ]
--- run.sh
+++ run.sh
@@ -12,12 +12,13 @@ fi
 if [ -f /commands/run ]; then
     echo running command /commands/run
     cat /commands/run
+    set -o pipefail
     sh -c /commands/run 2>&1 | tee /commands/output
-    echo done
     rc=$?
+    echo done with code $rc
     if [ $rc != 0 ]
     then
-      echo "error: $rc" | tee /commands/output
+      echo "error: $rc" | tee -a /commands/output
       touch /commands/code_error
     else
       touch /commands/code_ok
